f5e09168b6749221b45e526de1aab80179920461
zch130225 <58926172+zch130225@users.noreply.github.com>
2019-12-18 10:32:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,19 @@ wss.on('connection', ws => {
 
     ws.on('message', message => {
         console.log(`[SERVER] Received: ${message}`);
-
+         data= `${message}`;
         ws.send(`ECHO: ${message}`, err => {
             if (err) {
                 console.log(`[SERVER] error: ${err}`);
             }
         });
+        wss.clients.forEach(function each(client) {
+                  // 如果连接状态是打开状态,且不是当前客户端对象
+                  if (client !== ws && client.readyState === WebSocket.OPEN) {
+                    // 发送消息
+                    client.send( data);
+                  }
+                });
     });
 });
+
